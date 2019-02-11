bba2f9bf545d4278e4f48fb91348d9d51a0ab177
Miremad Aghili <37775995+maghili@users.noreply.github.com>
2019-02-11 11:35:00 -0500

    Update pharmacy-counting.py

--- src/pharmacy-counting.py
+++ src/pharmacy-counting.py
@@ -7,19 +7,16 @@ names = list(set([data[3] for data in D]))#the list of unique drug names
 drug_name_count_cost = {name:[0, 0] for name in names}
 #drug_name_count_cost = [[name, 0, 0] for name in names]
 for data in D:
-    #idx=names.index(data[3])#index of drug in the name list
-    #drug_name_count_cost[idx][1] += 1#counting ocurrance of each name
     drug_name_count_cost[data[3]][0] += 1
-    #drug_name_count_cost[idx][2] = drug_name_count_cost[idx][2]+int(data[-1])#adding the price
     drug_name_count_cost[data[3]][1] += int(data[-1])#adding the price
 
-
-#drug_name_count_cost.sort(key = lambda x: (x[-1],x[0]), reverse=True)#sorting by price and then name
+drug_name_count_cost_list = [[name, drug_name_count_cost[name][0] , drug_name_count_cost[name][1]]for name in drug_name_count_cost.keys()]
+drug_name_count_cost_list.sort(key = lambda x: (x[-1],x[0]), reverse=True)#sorting by price and then name
 
 f=open('./output/top_cost_drug.txt', 'w')
 f.write('drug_name,num_prescriber,total_cost')
 f.close()
-for data in drug_name_count_cost.keys():#writing the data in a text file
+for data in drug_name_count_cost_list:#writing the data in a text file
     f = open('./output/top_cost_drug.txt','a')
-    f.write('\n'+str(data)+','+str(drug_name_count_cost[data][0])+','+str(drug_name_count_cost[data][1]))
+    f.write('\n'+str(data[0])+','+str(data[1])+','+str(data[2]))
 f.close()
